dbd418c73949127880d7a3523194837b26c09e72
panos.mita2@outlook.com <pmita@dxc.com>
2021-05-25 10:31:07 +0100

    state, components, and styling added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 //Let's import our styling
 import './styles/app.scss';
+//Let's import our components
+import Cards from './Components/Cards';
+import items from './util_db';
 //Let's import everything Router related
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 function App() {
+  //Let's set our state
+  const [products, setProducts] = useState(items);
+  console.log(products);
   return (
     <div className="App">
       <h1>Test</h1>
+      <Cards 
+        products={products}
+      />
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = ({product}) => {
+    return(
+        <div>
+            <h1>Further Test</h1>
+        </div>
+    );
+}
+
+export default Card;
